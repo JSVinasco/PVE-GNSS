6ff7d954735ec1d6a054744762e569e8bb944307
jsvinasco <vinasco.juan@correounivalle.edu.co>
2021-08-06 16:00:41 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sub-sistema de Posicionamiento Global por Satelite (GNSS) para el proyecto denominado Desarrollo de un Prototipo Experimental de Vehículo Eléctrico en el Sector Automotriz del Valle del Cauca. Esto incluye diseño, he implementación de pruebas para caracterizar el vehiculo mediante datos GNSS, y productos relacionados.',
+    author='Andres Ordoñez, Juan Sebastian Vinasco, Pablo José Torres',
+    license='MIT',
+)
